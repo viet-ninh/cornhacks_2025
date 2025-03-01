8781b3dc55f8a77d51bd04ed1f154c6c1efb183c
eolson30 <eolson30@huskers.unl.edu>
2025-03-01 11:34:29 -0600

    Updated page to use 3 columns

---
+++ corn_popper/src/app/components/CornClick.tsx
@@ -0,0 +1,7 @@
+export default function CornClick() {
+    return (
+    <div>
+        <p>Test</p>
+    </div>
+    );
+}
--- corn_popper/src/app/page.tsx
+++ corn_popper/src/app/page.tsx
@@ -1,8 +1,18 @@
+import CornClick from './components/CornClick'
+import './globals.css'
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-      
+    <div className="flex_container">
+      <div className="flex_item">
+        <CornClick/>
+      </div>
+      <div className="flex_item">
+        {/* center screen goes here */}
+      </div>
+      <div className="flex_item">
+        {/* item_list goes here */}
+      </div>
     </div>
   );
 }
